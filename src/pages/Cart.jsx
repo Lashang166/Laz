9b2fa193fdeef617c20fd8bf1a0a1502e4bef72a
Lashang166 <bankteeradhol@gmail.com>
2021-02-09 13:08:49 +0700

    card

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Header } from '../components/Header'
+import CartItem from '../components/CartItem'
 import { Container, Grid, Paper } from '@material-ui/core'
 import Path from '../components/Path'
 
@@ -13,9 +14,7 @@ const Cart = () => {
             <Container className="mt-5">
                 <Grid container spacing={1}>
                     <Grid item xs={8}>
-                        <Paper className="p-5">
-                            asdf
-                        </Paper>
+                        <CartItem />
                     </Grid>
                     <Grid item xs={4}>
                         <Paper className="p-5">
