e10f45f8b902a0622dce63b799d2ba5dab80c4b1
Lashang166 <bankteeradhol@gmail.com>
2021-02-23 19:10:59 -0800

    fix

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get("/", function (req, res) {
     res.send("<h1>Hello World!</h1>")
   })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log("running");
 })
